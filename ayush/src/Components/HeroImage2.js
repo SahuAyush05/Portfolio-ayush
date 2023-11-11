686b657e61a15528a293c0e094a430acce2bad04
Ayush Sahu <ayushsahu050502@gmail.com>
2023-11-11 20:57:04 +0530

    01
    added home page , header, footer.

---
+++ ayush/src/Components/HeroImage2.js
@@ -0,0 +1,17 @@
+import './HeroImage2.css'
+
+import React from 'react'
+
+const HeroImage2 = () => {
+  return (
+    <div className='hero-img'>
+      <div className='heading'>
+        <h1></h1>
+        <p></p>
+      </div>
+
+    </div>
+  )
+}
+
+export default HeroImage2
---
+++ ayush/src/Routes/Project.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../Components/Navbar'
+import Footer from '../Components/Footer'
+import HeroImage2 from '../Components/HeroImage2'
+
+const Project = () => {
+  return (
+    <div>
+      <Navbar/>
+      <HeroImage2/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default Project
